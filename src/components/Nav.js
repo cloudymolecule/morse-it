3c551f18016498e8f13203a426f00e5ec0c01ddf
Cloudymolecule <baires82@gmail.com>
2021-09-26 13:41:03 -0400

    Add Nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { IconContext } from 'react-icons/lib'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import '../styles/Nav.css'
+
+function Nav() {
+
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => {
+        setClick(!click)
+    }
+
+    const closeMobileMenu = () => {
+        setClick(false)
+    }
+
+    return (
+        <>
+            <IconContext.Provider value={{ color: '#000' }}>
+                <div className='nav'>
+                    <div className='nav-container container'>
+                        <Link onClick={closeMobileMenu} to='/' className='nav-logo'>
+                            <div className='nav-name'>Morse-IT</div>
+                        </Link>
+                        <div onClick={handleClick} className='menu-icon'>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </div>
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                            <li className='nav-item'>
+                            </li>
+                            <li className='nav-item'>
+                            </li>
+                            <li className='nav-item'>
+                            </li>
+                            <li className='nav-item'>
+                            </li>
+                            <li className='nav-item'>
+                            </li>
+                            <li className='nav-item'>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </IconContext.Provider>
+        </>
+    )
+}
+
+export default Nav
