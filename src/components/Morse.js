4816edeeefbfb61395f3a3719db856a7a2486103
Cloudymolecule <baires82@gmail.com>
2021-09-29 09:56:31 -0400

    Add Home component

--- src/components/Morse.js
+++ src/components/Morse.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/Morse.css'
 
 function Morse() {
     return (
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,14 @@ import '../styles/App.css'
 import  { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Nav from '../components/Nav';
 import Morse from '../components/Morse';
+import Home from '../components/Home';
 
 function App() {
   return (
     <Router>
       <Nav />
       <Switch>
+        <Route path='/' exact component={Home} />
         <Route path='/morse' exact component={Morse} />
       </Switch>
     </Router>
