7d5a0ef73de5a33add434bbb2d8e485da839fd0d
Cloudymolecule <baires82@gmail.com>
2021-09-29 10:17:47 -0400

    Update Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,9 @@ function Home() {
     return (
         <div className='home-container'>
             <div className='home-title'>WELCOME TO MORSE-IT</div>
+            <div className='home-description'>
+                MORSE-IT is a simple web app that'll help you transelate any language to morse code. Give it a shot by clicking the Morse menu item.
+            </div>
         </div>
     )
 }
